6001ac3407e631a6d46a6229ded46c7c9d0f26e8
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-03 19:22:04 -0300

    feat: load profiles on player join

---
+++ src/server/services/profile-load.ts
@@ -0,0 +1,56 @@
+import { OnStart, Service } from "@flamework/core";
+import { GetProfileStore } from "@rbxts/profileservice";
+import { Profile, ProfileStore } from "@rbxts/profileservice/globals";
+import { PlayerData } from "@/shared/reflex/slices/players/types";
+import { defaultPlayerData } from "@/shared/reflex/slices/players/utils";
+import { OnPlayerAdd } from "@/server/services/lifecycles/on-player-add";
+import { Logger } from "@rbxts/log";
+import { Players } from "@rbxts/services";
+
+@Service()
+export class ProfileLoad implements OnStart, OnPlayerAdd {
+	private profileStore?: ProfileStore<PlayerData>;
+	private profiles = new Map<Player, Profile<PlayerData>>();
+
+	constructor(private logger: Logger) {}
+
+	onStart() {
+		this.profileStore = GetProfileStore({ Name: "PlayerData", Scope: "DEV" }, defaultPlayerData);
+	}
+
+	private waitForProfileStore() {
+		if (!this.profileStore) {
+			this.logger.Debug("Profile store is still loading");
+
+			while (!this.profileStore) {
+				task.wait();
+			}
+		}
+	}
+
+	onPlayerAdded(player: Player) {
+		this.waitForProfileStore();
+
+		const profile = this.profileStore!.LoadProfileAsync(`Data_${player.UserId}`, "ForceLoad");
+
+		if (!profile) {
+			player.Kick("todo label: Profile doesn't exist");
+			return;
+		}
+
+		profile.AddUserId(player.UserId);
+		profile.Reconcile();
+
+		profile.ListenToRelease(() => {
+			this.profiles.delete(player);
+			player.Kick("todo label: Profile released");
+		});
+
+		if (!player.IsDescendantOf(Players)) {
+			profile.Release();
+			return;
+		}
+
+		this.profiles.set(player, profile);
+	}
+}
