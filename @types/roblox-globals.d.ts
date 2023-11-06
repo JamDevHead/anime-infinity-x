0d94881c1ef978bc44a850132fde4941b7c86c6d
Revolution <tavinho.daniel@gmail.com>
2023-11-05 22:27:16 -0300

    fix: webstorm doesn't identify types

---
+++ @types/roblox-globals.d.ts
@@ -0,0 +1,19 @@
+/*
+ * WEBSTORM ISSUE TYPE FIX - DO NOT EDIT
+ */
+import "@rbxts/types/include/lua";
+import "@rbxts/types/include/macro_math";
+import "@rbxts/types/include/roblox";
+import "@rbxts/types/include/generated/enums";
+import "@rbxts/types/include/generated/None";
+import "@rbxts/compiler-types/types/Array";
+import "@rbxts/compiler-types/types/callMacros";
+import "@rbxts/compiler-types/types/core";
+import "@rbxts/compiler-types/types/eslintIgnore";
+import "@rbxts/compiler-types/types/Iterable";
+import "@rbxts/compiler-types/types/Map";
+import "@rbxts/compiler-types/types/Promise";
+import "@rbxts/compiler-types/types/Set";
+import "@rbxts/compiler-types/types/String";
+import "@rbxts/compiler-types/types/Symbol";
+import "@rbxts/compiler-types/types/typeUtils";
