91f54d34e0a0640beec9f70308ab9f8c58d514f0
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-08 18:15:21 -0300

    feat: enemy spawner

---
+++ src/server/components/enemy-spawner.ts
@@ -0,0 +1,80 @@
+import { BaseComponent, Component } from "@flamework/components";
+import { OnStart } from "@flamework/core";
+import { Logger } from "@rbxts/log";
+import { HttpService, ServerStorage } from "@rbxts/services";
+import { EnemyModel } from "@/server/components/enemy";
+import { EnemiesSpawner } from "@/server/services/enemies-spawner";
+
+interface EnemyAttributes {
+	EnemyZone: string;
+	AllowedEnemies: string | undefined;
+}
+
+@Component({ tag: "EnemySpawner" })
+export class Enemy extends BaseComponent<EnemyAttributes, Part> implements OnStart {
+	private currentEnemy: EnemyModel | undefined;
+	private enemiesZone = ServerStorage.assets.Avatars.NPCsModels.FindFirstChild(this.attributes.EnemyZone);
+	private spawnerSurfaceCFrame = this.instance.CFrame.sub(Vector3.yAxis.mul(this.instance.Size.Y / 2));
+
+	constructor(
+		private readonly logger: Logger,
+		private readonly enemiesSpawner: EnemiesSpawner,
+	) {
+		super();
+	}
+
+	onStart() {
+		assert(this.enemiesZone !== undefined, `Failed to get enemy zone: ${this.attributes.EnemyZone}`);
+
+		this.instance.Anchored = true;
+		this.instance.CanCollide = false;
+		this.instance.CanQuery = false;
+		this.instance.CanTouch = false;
+		this.instance.Transparency = 1;
+
+		const enemy = this.getEnemy();
+
+		if (enemy) {
+			this.spawn(enemy);
+		}
+	}
+
+	getEnemy(): EnemyModel | undefined {
+		let enemies = this.enemiesZone?.GetChildren() ?? [];
+
+		if (this.attributes.AllowedEnemies !== undefined) {
+			const allowedEnemies = this.attributes.AllowedEnemies.split(",");
+			enemies = enemies.filter(
+				(enemy) => allowedEnemies.find((allowedEnemy) => allowedEnemy === enemy.Name) !== undefined,
+			);
+		}
+
+		if (enemies.isEmpty()) {
+			this.logger.Debug("Enemy array list is empty");
+			return undefined;
+		}
+
+		const enemy = enemies[math.random(1, enemies.size())] as EnemyModel | undefined;
+
+		if (!enemy) {
+			return this.getEnemy();
+		}
+
+		return enemy;
+	}
+
+	spawn(enemy: EnemyModel) {
+		const clonedEnemy = enemy.Clone();
+		const rootCFrame = clonedEnemy.HumanoidRootPart.GetPivot();
+
+		clonedEnemy.PrimaryPart = undefined;
+		clonedEnemy.WorldPivot = rootCFrame.sub(Vector3.yAxis.mul(3));
+		clonedEnemy.PivotTo(this.spawnerSurfaceCFrame);
+		clonedEnemy.SetAttribute("Guid", HttpService.GenerateGUID(false));
+		clonedEnemy.AddTag("EnemyNPC");
+		clonedEnemy.Parent = this.enemiesSpawner.enemiesFolder;
+
+		this.currentEnemy?.Destroy();
+		this.currentEnemy = clonedEnemy;
+	}
+}
---
+++ src/server/services/enemies-spawner.ts
@@ -0,0 +1,35 @@
+import { OnStart, Service } from "@flamework/core";
+import { ServerStorage, Workspace } from "@rbxts/services";
+import { ZonesLoader } from "./zones-loader";
+import { Logger } from "@rbxts/log";
+
+@Service()
+export class EnemiesSpawner implements OnStart {
+	public enemiesFolder = new Instance("Folder");
+
+	constructor(
+		private logger: Logger,
+		private readonly zonesLoader: ZonesLoader,
+	) {}
+
+	onStart() {
+		const npcModels = ServerStorage.assets.Avatars.NPCsModels;
+
+		this.enemiesFolder.Name = "Enemies";
+		this.enemiesFolder.Parent = Workspace;
+
+		for (const zone of this.zonesLoader.zonesFolder.GetChildren()) {
+			const npcZone = npcModels.FindFirstChild(zone.Name);
+
+			if (npcZone === undefined) {
+				this.logger.Warn("Failed to find npc zone {npcZone}", zone.Name);
+				continue;
+			}
+
+			for (const node of zone.Nodes.GetChildren()) {
+				node.SetAttribute("EnemyZone", zone.Name);
+				node.AddTag("EnemySpawner");
+			}
+		}
+	}
+}
