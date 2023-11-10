2f6162fdf37e962ccc72bb4fee3f7df0e0e67845
Revolution <tavinho.daniel@gmail.com>
2023-11-10 19:04:37 -0300

    feat: add useRootProducer type for react-reflex

--- src/client/reflex/producers/index.ts
+++ src/client/reflex/producers/index.ts
@@ -1,3 +1,4 @@
+import { useProducer, UseProducerHook, useSelector, UseSelectorHook } from "@rbxts/react-reflex";
 import { combineProducers, InferState } from "@rbxts/reflex";
 import { slices } from "shared/reflex/slices";
 
@@ -7,3 +8,6 @@ export type RootState = InferState<RootProducer>;
 export const producer = combineProducers({
 	...slices,
 });
+
+export const useRootProducer: UseProducerHook<RootProducer> = useProducer;
+export const useRootSelector: UseSelectorHook<RootProducer> = useSelector;
