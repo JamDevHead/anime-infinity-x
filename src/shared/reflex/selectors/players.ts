63e48b91e86efbb41a99e306557d7286850cdf6e
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-10 14:40:40 -0300

    feat: fighters reflex slice

--- src/shared/reflex/selectors/players.ts
+++ src/shared/reflex/selectors/players.ts
@@ -1,5 +1,5 @@
-import { SharedState } from "@/shared/reflex/slices";
 import { createSelector } from "@rbxts/reflex";
+import { SharedState } from "@/shared/reflex/slices";
 import { PlayerData } from "@/shared/reflex/slices/players/types";
 
 export const selectPlayerBalance = (playerId: string) => {
@@ -14,16 +14,23 @@ export const selectPlayerZones = (playerId: string) => {
 	};
 };
 
+export const selectPlayerFighters = (playerId: string) => {
+	return (state: SharedState) => {
+		return state.players.fighters[playerId];
+	};
+};
+
 export const selectPlayerData = (playerId: string) => {
 	return createSelector(
 		selectPlayerBalance(playerId),
 		selectPlayerZones(playerId),
-		(balance, zones): PlayerData | undefined => {
-			if (!balance || !zones) {
+		selectPlayerFighters(playerId),
+		(balance, zones, fighters): PlayerData | undefined => {
+			if (!balance || !zones || !fighters) {
 				return;
 			}
 
-			return { boosts: {}, fighters: {}, inventory: {}, missions: {}, settings: {}, balance, zones };
+			return { boosts: {}, fighters, inventory: {}, missions: {}, settings: {}, balance, zones };
 		},
 	);
 };
--- src/shared/reflex/slices/players/index.ts
+++ src/shared/reflex/slices/players/index.ts
@@ -1,8 +1,10 @@
 import { combineProducers } from "@rbxts/reflex";
 import { balanceSlice } from "@/shared/reflex/slices/players/balance";
+import { fightersSlice } from "@/shared/reflex/slices/players/fighters";
 import { zonesSlice } from "@/shared/reflex/slices/players/zones";
 
 export const playersSlice = combineProducers({
 	balance: balanceSlice,
 	zones: zonesSlice,
+	fighters: fightersSlice,
 });
