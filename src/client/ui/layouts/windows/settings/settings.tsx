1a69367598ebfa9ae58c59c62450f64320215fac
Revolution <tavinho.daniel@gmail.com>
2023-11-14 19:28:13 -0300

    feat: add option background on settings window

--- src/client/ui/layouts/windows/settings/settings.tsx
+++ src/client/ui/layouts/windows/settings/settings.tsx
@@ -2,12 +2,14 @@ import Object from "@rbxts/object-utils";
 import Roact from "@rbxts/roact";
 import { fonts } from "@/client/constants/fonts";
 import { useRootProducer, useRootSelector } from "@/client/reflex/producers";
+import { Image } from "@/client/ui/components/image";
 import { ScrollView } from "@/client/ui/components/scroll-view";
 import { Stack } from "@/client/ui/components/stack";
 import { Text } from "@/client/ui/components/text";
 import { ToggleOption } from "@/client/ui/components/toggle-option";
 import { Window } from "@/client/ui/components/window";
 import { useRem } from "@/client/ui/hooks/use-rem";
+import { images } from "@/shared/assets/images";
 
 export const Settings = () => {
 	const rem = useRem();
@@ -21,7 +23,7 @@ export const Settings = () => {
 				fillDirection="Vertical"
 				verticalAlignment="Top"
 				horizontalAlignment="Center"
-				padding={new UDim(0, 24)}
+				padding={new UDim(0, 32)}
 			>
 				<Text
 					text="~ General ~"
@@ -30,28 +32,31 @@ export const Settings = () => {
 					size={UDim2.fromScale(1, 0)}
 					textColor={Color3.fromRGB(255, 255, 255)}
 				/>
-				<ScrollView size={UDim2.fromScale(1, 1)}>
+				<ScrollView size={UDim2.fromScale(1, 1)} padding={new UDim(0, 8)}>
 					{Object.entries(settings).map(([key, value]) => (
-						<Stack
-							key={key}
-							size={UDim2.fromScale(1, 0.2)}
-							fillDirection="Horizontal"
-							verticalAlignment="Center"
-						>
-							<Text
-								text={value.label}
-								font={fonts.inter.bold}
-								textSize={rem(2)}
-								size={UDim2.fromScale(0.8, 1)}
-								textColor={Color3.fromRGB(255, 255, 255)}
-							/>
-							{typeOf(value.value) === "boolean" && (
-								<ToggleOption
-									checked={value.value as boolean}
-									onChange={(value) => dispatch.setSetting(key as never, value)}
+						<Image key={key} size={UDim2.fromScale(1, 0.2)} image={images.ui.option_background}>
+							<Stack size={UDim2.fromScale(1, 1)} fillDirection="Horizontal" verticalAlignment="Center">
+								<Text
+									text={value.label}
+									font={fonts.inter.bold}
+									textSize={rem(2)}
+									size={UDim2.fromScale(0.8, 1)}
+									textColor={Color3.fromRGB(255, 255, 255)}
 								/>
-							)}
-						</Stack>
+								{typeOf(value.value) === "boolean" && (
+									<ToggleOption
+										checked={value.value as boolean}
+										onChange={(value) => dispatch.setSetting(key as never, value)}
+									/>
+								)}
+							</Stack>
+							<uipadding
+								PaddingLeft={new UDim(0, rem(2.5))}
+								PaddingRight={new UDim(0, rem(2.5))}
+								PaddingTop={new UDim(0, rem(2.5))}
+								PaddingBottom={new UDim(0, rem(2.5))}
+							/>
+						</Image>
 					))}
 				</ScrollView>
 			</Stack>
