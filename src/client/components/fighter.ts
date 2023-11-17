c9ef2b88e6e3fdadabc1b12e9840bba5b99959b2
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-17 00:53:02 -0300

    feat: fighter component

---
+++ src/client/components/fighter.ts
@@ -0,0 +1,124 @@
+import { BaseComponent, Component } from "@flamework/components";
+import { OnRender, OnStart } from "@flamework/core";
+import { CharacterAdd } from "@/client/controllers/lifecycles/on-character-add";
+import { Trove } from "@rbxts/trove";
+import { Logger } from "@rbxts/log";
+
+interface FighterModel extends Model {
+	Humanoid: Humanoid & {
+		Animator: Animator;
+	};
+}
+
+const animationMap = {
+	idle: "180435571",
+	walk: "180426354",
+	run: "180426354",
+	jump: "125750702",
+	fall: "180436148",
+} as const;
+
+@Component({ tag: "Fighter" })
+export class Fighter extends BaseComponent<{}, FighterModel> implements OnStart, OnRender {
+	private humanoid = this.instance.Humanoid;
+	private animator = this.humanoid.Animator;
+	private trove = new Trove();
+	private animationCache = new Map<string, AnimationTrack>();
+
+	constructor(
+		private readonly logger: Logger,
+		private readonly characterAdd: CharacterAdd,
+	) {
+		super();
+	}
+
+	onStart() {
+		this.logger.Debug("Created animator handler for fighter");
+
+		this.trove.add(() => {
+			this.animationCache.forEach((track) => track.Destroy());
+			this.animationCache.clear();
+		});
+	}
+
+	onRender(dt: number) {
+		const character = this.characterAdd.character;
+		const humanoid = character?.FindFirstChild("Humanoid") as Humanoid | undefined;
+
+		if (!humanoid?.RootPart) {
+			return;
+		}
+
+		const isFalling = humanoid.FloorMaterial === Enum.Material.Air;
+		const isJumping = humanoid.Jump;
+		const isRunning = humanoid.RootPart.AssemblyLinearVelocity.mul(new Vector3(1, 0, 1)).Magnitude > 0.03;
+
+		switch (true) {
+			case isFalling:
+				this.swapAnimation("fall");
+				break;
+			case isJumping:
+				this.swapAnimation("jump");
+				break;
+			case isRunning:
+				this.swapAnimation("run");
+				break;
+			default:
+				this.swapAnimation("idle");
+				break;
+		}
+	}
+
+	destroy() {
+		super.destroy();
+		this.trove.destroy();
+	}
+
+	private swapAnimation(name: keyof typeof animationMap) {
+		this.stopAllAnimations(name);
+		this.playAnimationTrack(name);
+	}
+
+	private playAnimationTrack(name: keyof typeof animationMap) {
+		const fighterTrack = this.getAnimationTrack(name);
+
+		if (fighterTrack && !fighterTrack.IsPlaying) {
+			fighterTrack.Play();
+		}
+	}
+
+	private stopAllAnimations(exception?: keyof typeof animationMap) {
+		this.animationCache.forEach((track) => {
+			if (exception === track.Name) {
+				return;
+			}
+
+			track.Stop();
+		});
+	}
+
+	private getAnimationTrack(name: keyof typeof animationMap) {
+		const id = animationMap[name];
+
+		if (!id) {
+			return;
+		}
+
+		const cachedTrack = this.animationCache.get(id);
+
+		if (cachedTrack) {
+			return cachedTrack;
+		}
+
+		const animationInstance = new Instance("Animation");
+		animationInstance.AnimationId = `http://www.roblox.com/asset/?id=${id}`;
+		animationInstance.Name = name;
+		animationInstance.Parent = this.animator;
+
+		const track = this.animator.LoadAnimation(animationInstance);
+
+		this.animationCache.set(id, track);
+
+		return track;
+	}
+}
