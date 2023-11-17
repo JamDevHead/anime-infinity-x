64ca4979470b38c252ebe48444077ba59b98090b
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-17 00:54:22 -0300

    feat: fighter slice

--- src/server/services/profile-load/loaders/fighters.ts
+++ src/server/services/profile-load/loaders/fighters.ts
@@ -1,20 +1,23 @@
 import { Profile } from "@rbxts/profileservice/globals";
-import { HttpService, ServerStorage } from "@rbxts/services";
+import { HttpService, ReplicatedStorage } from "@rbxts/services";
+import { producer } from "@/server/reflex/producers";
 import { PlayerData } from "@/shared/reflex/slices/players/types";
 
-const initialFighter = ServerStorage.assets.Avatars.FightersModels.NRT.TestFighter;
+const initialFighter = ReplicatedStorage.assets.Avatars.FightersModels.NRT.Bro;
 
 export default function loadFighters(profile: Profile<PlayerData>) {
 	const fighters = profile.Data.fighters;
+	const userId = tostring(profile.UserIds.shift());
 
 	if (fighters.all.size() === 0) {
 		const uid = HttpService.GenerateGUID(false);
-		fighters.all.push({
+
+		producer.addFighter(userId, uid, {
 			name: initialFighter.Name,
 			level: 0,
 			zone: initialFighter.Parent!.Name,
-			uid,
 		});
-		fighters.actives.push(uid);
+
+		producer.setActiveFighter(userId, uid);
 	}
 }
--- src/shared/reflex/slices/players/fighters.ts
+++ src/shared/reflex/slices/players/fighters.ts
@@ -1,5 +1,5 @@
 import { createProducer } from "@rbxts/reflex";
-import { PlayerData, PlayerFighters } from "@/shared/reflex/slices/players/types";
+import { PlayerData, PlayerFighter, PlayerFighters } from "@/shared/reflex/slices/players/types";
 
 interface FighterState {
 	readonly [playerId: string]: PlayerFighters | undefined;
@@ -17,4 +17,68 @@ export const fightersSlice = createProducer(initialState, {
 		...state,
 		[playerId]: undefined,
 	}),
+
+	addFighter: (state, playerId: string, fighterUid: string, playerFighterData: Omit<PlayerFighter, "uid">) => {
+		const playerData = state[playerId];
+
+		if (!playerData) {
+			return state;
+		}
+
+		return {
+			...state,
+			[playerId]: {
+				all: [...playerData.all, { uid: fighterUid, ...playerFighterData }],
+				actives: playerData.actives,
+			},
+		};
+	},
+
+	removeFighter: (state, playerId: string, fighterUid: string) => {
+		const playerData = state[playerId];
+
+		if (!playerData) {
+			return state;
+		}
+
+		return {
+			...state,
+			[playerId]: {
+				all: playerData.all.filter((fighter) => fighter.uid !== fighterUid),
+				actives: playerData.actives,
+			},
+		};
+	},
+
+	setActiveFighter: (state, playerId: string, fighterUid: string) => {
+		const playerData = state[playerId];
+
+		if (!playerData) {
+			return state;
+		}
+
+		return {
+			...state,
+			[playerId]: {
+				all: playerData.all,
+				actives: [...playerData.actives, fighterUid],
+			},
+		};
+	},
+
+	removeActiveFighter: (state, playerId: string, fighterUid: string) => {
+		const playerData = state[playerId];
+
+		if (!playerData) {
+			return state;
+		}
+
+		return {
+			...state,
+			[playerId]: {
+				all: playerData.all,
+				actives: playerData.actives.filter((activeFighter) => activeFighter !== fighterUid),
+			},
+		};
+	},
 });
