1edf651b9b5b7dbba05db5e8d1c482e370f8f3ab
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-17 16:47:58 -0300

    feat: disable fighter torso collision

--- src/client/components/fighter-goal.ts
+++ src/client/components/fighter-goal.ts
@@ -12,218 +12,225 @@ import { selectPlayerFighters } from "@/shared/reflex/selectors";
 import { PlayerFighter } from "@/shared/reflex/slices/players/types";
 
 @Component({
-  tag: "FighterGoal",
+	tag: "FighterGoal",
 })
 export class FighterGoal extends BaseComponent<{ UID: string }, Attachment> implements OnStart, OnRender {
-  private fighterPart = new Instance("Part");
-  private root = this.instance.Parent as Part | undefined;
-  private raycastParams = new RaycastParams();
-  private trove = new Trove();
-  private fighterModel: Model | undefined;
+	private fighterPart = new Instance("Part");
+	private root = this.instance.Parent as Part | undefined;
+	private raycastParams = new RaycastParams();
+	private trove = new Trove();
+	private fighterModel: Model | undefined;
 
-  constructor(
-    private readonly logger: Logger,
-    private readonly characterAdd: CharacterAdd,
-    private readonly fighterTracker: FightersTracker,
-  ) {
-    super();
-  }
+	constructor(
+		private readonly logger: Logger,
+		private readonly characterAdd: CharacterAdd,
+		private readonly fighterTracker: FightersTracker,
+	) {
+		super();
+	}
 
-  onStart() {
-    const localPlayer = Players.LocalPlayer;
+	onStart() {
+		const localPlayer = Players.LocalPlayer;
 
-    this.fighterPart.Name = "FighterPart";
-    this.fighterPart.Anchored = true;
-    this.fighterPart.CanCollide = false;
-    this.fighterPart.CanQuery = false;
-    this.fighterPart.CanTouch = false;
-    this.fighterPart.CastShadow = false;
-    this.fighterPart.Size = Vector3.one;
-    this.fighterPart.Transparency = 1;
+		this.fighterPart.Name = "FighterPart";
+		this.fighterPart.Anchored = true;
+		this.fighterPart.CanCollide = false;
+		this.fighterPart.CanQuery = false;
+		this.fighterPart.CanTouch = false;
+		this.fighterPart.CastShadow = false;
+		this.fighterPart.Size = Vector3.one;
+		this.fighterPart.Transparency = 1;
 
-    this.fighterPart.Parent = this.instance;
+		this.fighterPart.Parent = this.instance;
 
-    this.instance.Visible = true;
+		this.instance.Visible = true;
 
-    this.raycastParams.FilterType = Enum.RaycastFilterType.Exclude;
-    if (this.root?.Parent) {
-      this.raycastParams.AddToFilter(this.root.Parent);
-    }
-    this.raycastParams.AddToFilter(this.fighterTracker.fightersFolder);
+		this.raycastParams.FilterType = Enum.RaycastFilterType.Exclude;
+		if (this.root?.Parent) {
+			this.raycastParams.AddToFilter(this.root.Parent);
+		}
+		this.raycastParams.AddToFilter(this.fighterTracker.fightersFolder);
 
-    const selectFighter = (playerId: string, uid: string) => {
-      return createSelector(selectPlayerFighters(playerId), (fighters) => {
-        const fighter = fighters?.all.find((fighter) => fighter.uid === uid);
-        print(`selecting`, fighters, fighter);
-        return fighter;
-      });
-    };
+		const selectFighter = (playerId: string, uid: string) => {
+			return createSelector(selectPlayerFighters(playerId), (fighters) => {
+				const fighter = fighters?.all.find((fighter) => fighter.uid === uid);
+				print(`selecting`, fighters, fighter);
+				return fighter;
+			});
+		};
 
-    const doesFighterExists = (fighter: PlayerFighter | undefined): fighter is PlayerFighter =>
-      fighter !== undefined;
-    const fighterSelector = selectFighter(tostring(localPlayer.UserId), this.attributes.UID);
+		const doesFighterExists = (fighter: PlayerFighter | undefined): fighter is PlayerFighter =>
+			fighter !== undefined;
+		const fighterSelector = selectFighter(tostring(localPlayer.UserId), this.attributes.UID);
 
-    this.logger.Debug("Fighter goal created UID: {uid}", this.attributes.UID);
+		this.logger.Debug("Fighter goal created UID: {uid}", this.attributes.UID);
 
-    const fighter = producer.getState(fighterSelector);
+		const fighter = producer.getState(fighterSelector);
 
-    if (fighter) {
-      this.onFighterCreation(fighter);
-    } else {
-      this.trove.add(
-        producer.once(fighterSelector, doesFighterExists, (fighter) =>
-          this.onFighterCreation(fighter as PlayerFighter),
-        ),
-      );
-    }
-  }
+		if (fighter) {
+			this.onFighterCreation(fighter);
+		} else {
+			this.trove.add(
+				producer.once(fighterSelector, doesFighterExists, (fighter) =>
+					this.onFighterCreation(fighter as PlayerFighter),
+				),
+			);
+		}
+	}
 
-  destroy() {
-    super.destroy();
-    this.trove.destroy();
-  }
+	destroy() {
+		super.destroy();
+		this.trove.destroy();
+	}
 
-  onRender(dt: number) {
-    this.updateFighterGoal(dt);
-    this.updateFighterModel();
-  }
+	onRender(dt: number) {
+		this.updateFighterGoal(dt);
+		this.updateFighterModel();
+	}
 
-  private onFighterCreation(fighter: PlayerFighter) {
-    const fighterZone = ReplicatedStorage.assets.Avatars.FightersModels.FindFirstChild(fighter.zone);
-    const fighterModel = fighterZone?.FindFirstChild(fighter.name)?.Clone() as Model | undefined;
+	private onFighterCreation(fighter: PlayerFighter) {
+		const fighterZone = ReplicatedStorage.assets.Avatars.FightersModels.FindFirstChild(fighter.zone);
+		const fighterModel = fighterZone?.FindFirstChild(fighter.name)?.Clone() as Model | undefined;
 
-    if (!fighterModel) {
-      this.logger.Warn("Failed to find fighter model {zone} {name}", fighter.zone, fighter.name);
-      return;
-    }
+		if (!fighterModel) {
+			this.logger.Warn("Failed to find fighter model {zone} {name}", fighter.zone, fighter.name);
+			return;
+		}
 
-    fighterModel.Parent = this.fighterTracker.fightersFolder;
+		fighterModel.Parent = this.fighterTracker.fightersFolder;
 
-    task.defer(() => {
-      // Cleanup fighter model
-      for (const part of fighterModel.GetDescendants()) {
-        if (part.IsA("BasePart")) {
-          part.CanCollide = false;
-          part.CanQuery = false;
-          part.Anchored = false;
-          part.CanTouch = false;
-        }
-      }
-    });
+		task.defer(() => {
+			// Cleanup fighter model
+			for (const part of fighterModel.GetDescendants()) {
+				if (part.IsA("BasePart")) {
+					part.CanCollide = false;
+					part.CanQuery = false;
+					part.Anchored = false;
+					part.CanTouch = false;
+				}
+			}
+		});
 
-    fighterModel.AddTag("Fighter");
+		fighterModel.AddTag("Fighter");
 
-    this.logger.Debug("fighter created: {@fighter}", fighter);
+		this.logger.Debug("fighter created: {@fighter}", fighter);
 
-    this.fighterModel = fighterModel;
-    this.updateFighterGoal(10);
-  }
+		this.fighterModel = fighterModel;
+		this.updateFighterGoal(10);
+	}
 
-  private updateFighterModel() {
-    if (!this.fighterModel) {
-      return;
-    }
+	private updateFighterModel() {
+		if (!this.fighterModel) {
+			return;
+		}
 
-    const fighterPartSize = this.fighterPart.Size.Y / 2;
+		const fighterPartSize = this.fighterPart.Size.Y / 2;
 
-    this.fighterModel.PivotTo(this.fighterPart.CFrame.add(Vector3.yAxis.mul(3 - fighterPartSize)));
-  }
+		this.fighterModel.PivotTo(this.fighterPart.CFrame.add(Vector3.yAxis.mul(3 - fighterPartSize)));
 
-  private updateFighterGoal(dt: number) {
-    const goal = this.instance.WorldPosition;
-    const newGoal = this.getOcclusionResult(goal);
-    const groundResult = newGoal && this.getGroundResult(newGoal);
+		// Disable torso collision
+		const torso = this.fighterModel.FindFirstChild("Torso") as BasePart | undefined;
 
-    if (!newGoal || !groundResult) {
-      return;
-    }
+		if (torso) {
+			torso.CanCollide = false;
+		}
+	}
 
-    const character = this.characterAdd.character;
-    const humanoid = character?.FindFirstChild("Humanoid") as Humanoid | undefined;
-    const isFloating = humanoid?.FloorMaterial === Enum.Material.Air;
+	private updateFighterGoal(dt: number) {
+		const goal = this.instance.WorldPosition;
+		const newGoal = this.getOcclusionResult(goal);
+		const groundResult = newGoal && this.getGroundResult(newGoal);
 
-    const rootPosition = this.root?.Position.add(this.root?.CFrame.LookVector.mul(6)) ?? goal;
-    const finalGoal = new Vector3(newGoal.X, isFloating ? goal.Y : groundResult.Position.Y, newGoal.Z);
-    const goalLookAt = new Vector3(rootPosition.X, finalGoal.Y, rootPosition.Z);
+		if (!newGoal || !groundResult) {
+			return;
+		}
 
-    // Lerp part to origin
-    this.fighterPart.CFrame = this.fighterPart.CFrame.Lerp(new CFrame(finalGoal, goalLookAt), dt * 10);
-  }
+		const character = this.characterAdd.character;
+		const humanoid = character?.FindFirstChild("Humanoid") as Humanoid | undefined;
+		const isFloating = humanoid?.FloorMaterial === Enum.Material.Air;
 
-  private getOcclusionResult(goal: Vector3) {
-    if (!this.root) {
-      return;
-    }
+		const rootPosition = this.root?.Position.add(this.root?.CFrame.LookVector.mul(6)) ?? goal;
+		const finalGoal = new Vector3(newGoal.X, isFloating ? goal.Y : groundResult.Position.Y, newGoal.Z);
+		const goalLookAt = new Vector3(rootPosition.X, finalGoal.Y, rootPosition.Z);
 
-    const origin = this.root.Position;
-    const direction = goal.sub(origin);
+		// Lerp part to origin
+		this.fighterPart.CFrame = this.fighterPart.CFrame.Lerp(new CFrame(finalGoal, goalLookAt), dt * 10);
+	}
 
-    const backwardsDirection = this.root.CFrame.LookVector.Unit.mul(-1);
-    const angleCos = math.acos(direction.Unit.Dot(backwardsDirection));
+	private getOcclusionResult(goal: Vector3) {
+		if (!this.root) {
+			return;
+		}
 
-    const backLength = (direction.Magnitude * angleCos) / 2;
-    const backDirection = backwardsDirection.mul(backLength);
-    const backPosition = origin.add(backDirection);
+		const origin = this.root.Position;
+		const direction = goal.sub(origin);
 
-    const rightDirection = goal.sub(backPosition);
+		const backwardsDirection = this.root.CFrame.LookVector.Unit.mul(-1);
+		const angleCos = math.acos(direction.Unit.Dot(backwardsDirection));
 
-    let backResult = Workspace.Raycast(origin, backDirection, this.raycastParams);
-    let result = goal;
+		const backLength = (direction.Magnitude * angleCos) / 2;
+		const backDirection = backwardsDirection.mul(backLength);
+		const backPosition = origin.add(backDirection);
 
-    if (!backResult) {
-      backResult = {
-        Position: backPosition,
-      } as RaycastResult;
-    }
+		const rightDirection = goal.sub(backPosition);
 
-    let rightResult = Workspace.Raycast(
-      backResult.Position.sub(rightDirection.Unit.mul(1.5)),
-      rightDirection.mul(1.5),
-      this.raycastParams,
-    );
+		let backResult = Workspace.Raycast(origin, backDirection, this.raycastParams);
+		let result = goal;
 
-    if (!rightResult) {
-      rightResult = {
-        Position: backResult.Position.add(rightDirection),
-      } as RaycastResult;
-    }
+		if (!backResult) {
+			backResult = {
+				Position: backPosition,
+			} as RaycastResult;
+		}
 
-    const purple = {
-      color: Color3.fromRGB(160, 32, 240),
-    };
+		let rightResult = Workspace.Raycast(
+			backResult.Position.sub(rightDirection.Unit.mul(1.5)),
+			rightDirection.mul(1.5),
+			this.raycastParams,
+		);
 
-    Gizmo.arrow.styleDraw(purple, origin, backResult.Position);
+		if (!rightResult) {
+			rightResult = {
+				Position: backResult.Position.add(rightDirection),
+			} as RaycastResult;
+		}
 
-    Gizmo.arrow.styleDraw(purple, backResult.Position.sub(rightDirection.Unit.mul(1.5)), rightResult.Position);
+		const purple = {
+			color: Color3.fromRGB(160, 32, 240),
+		};
 
-    Gizmo.arrow.styleDraw(
-      {
-        color: Color3.fromRGB(255, 0, 0),
-      },
-      origin,
-      rightResult?.Position ?? goal,
-    );
+		Gizmo.arrow.styleDraw(purple, origin, backResult.Position);
 
-    if (rightResult?.Normal) {
-      result = rightResult.Position.sub(rightResult.Normal.mul(-1));
-    }
+		Gizmo.arrow.styleDraw(purple, backResult.Position.sub(rightDirection.Unit.mul(1.5)), rightResult.Position);
 
-    return result;
-  }
+		Gizmo.arrow.styleDraw(
+			{
+				color: Color3.fromRGB(255, 0, 0),
+			},
+			origin,
+			rightResult?.Position ?? goal,
+		);
 
-  private getGroundResult(goal: Vector3) {
-    const upRaycast = Workspace.Raycast(goal, Vector3.yAxis.mul(100), this.raycastParams);
-    let origin = goal.add(Vector3.yAxis.mul(2));
+		if (rightResult?.Normal) {
+			result = rightResult.Position.sub(rightResult.Normal.mul(-1));
+		}
 
-    if (upRaycast) {
-      origin = upRaycast.Position;
-    }
+		return result;
+	}
 
-    const downRaycast = Workspace.Raycast(origin, Vector3.yAxis.mul(-100), this.raycastParams);
+	private getGroundResult(goal: Vector3) {
+		const upRaycast = Workspace.Raycast(goal, Vector3.yAxis.mul(100), this.raycastParams);
+		let origin = goal.add(Vector3.yAxis.mul(2));
 
-    Gizmo.arrow.draw(goal, origin);
-    Gizmo.arrow.draw(origin, downRaycast?.Position ?? origin.add(Vector3.yAxis.mul(-100)));
+		if (upRaycast) {
+			origin = upRaycast.Position;
+		}
 
-    return downRaycast;
-  }
+		const downRaycast = Workspace.Raycast(origin, Vector3.yAxis.mul(-100), this.raycastParams);
+
+		Gizmo.arrow.draw(goal, origin);
+		Gizmo.arrow.draw(origin, downRaycast?.Position ?? origin.add(Vector3.yAxis.mul(-100)));
+
+		return downRaycast;
+	}
 }
