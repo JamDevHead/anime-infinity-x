be279de9066bbaa7b3875318def9c9d3575f1891
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-17 18:56:38 -0300

    feat: apply reflex devtools

--- src/client/reflex/producers/index.ts
+++ src/client/reflex/producers/index.ts
@@ -1,21 +1,16 @@
 import { useProducer, UseProducerHook, useSelector, UseSelectorHook } from "@rbxts/react-reflex";
 import { combineProducers, InferState } from "@rbxts/reflex";
 import { slices } from "shared/reflex/slices";
-import { dpsSlice } from "@/client/reflex/slices/dps";
-import { hudSlice } from "@/client/reflex/slices/hud";
-import { settingsSlice } from "@/client/reflex/slices/settings";
-import { windowSlice } from "@/client/reflex/slices/window";
+import { devToolsMiddleware } from "@/client/reflex/producers/middlewares/devtools";
+import { clientSlices } from "@/client/reflex/slices";
 
 export type RootProducer = typeof producer;
 export type RootState = InferState<RootProducer>;
 
 export const producer = combineProducers({
 	...slices,
-	dps: dpsSlice,
-	hud: hudSlice,
-	settings: settingsSlice,
-	window: windowSlice,
-});
+	...clientSlices,
+}).applyMiddleware(devToolsMiddleware);
 
 export const useRootProducer: UseProducerHook<RootProducer> = useProducer;
 export const useRootSelector: UseSelectorHook<RootProducer> = useSelector;
