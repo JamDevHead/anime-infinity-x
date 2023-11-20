d0ed80baf044f8770ae7243a6c0305fa1182d596
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-20 11:13:52 -0300

    chore: generate 1 test fighter only

--- src/server/services/profile-load/loaders/fighters.ts
+++ src/server/services/profile-load/loaders/fighters.ts
@@ -1,7 +1,6 @@
 import { Profile } from "@rbxts/profileservice/globals";
 import { HttpService, ReplicatedStorage } from "@rbxts/services";
 import { producer } from "@/server/reflex/producers";
-import { selectPlayerFighters } from "@/shared/reflex/selectors";
 import { PlayerData } from "@/shared/reflex/slices/players/types";
 
 const initialFighter = ReplicatedStorage.assets.Avatars.FightersModels.NRT;
@@ -11,34 +10,14 @@ export default function loadFighters(profile: Profile<PlayerData>) {
 	const userId = tostring(profile.UserIds.shift());
 
 	if (fighters.all.size() === 0) {
-		function create() {
-			const uid = HttpService.GenerateGUID(false);
+		const uid = HttpService.GenerateGUID(false);
 
-			producer.addFighter(userId, uid, {
-				name: initialFighter.Oro.Name,
-				level: 0,
-				zone: initialFighter.Name,
-			});
-
-			producer.addActiveFighter(userId, uid);
-		}
-
-		task.spawn(() => {
-			for (const _i of $range(1, 30)) {
-				const playerFighters = producer.getState(selectPlayerFighters(userId));
-				task.wait(1.5);
-				create();
-				task.wait(1.5);
-				if (playerFighters && playerFighters.all.size() > 5) {
-					playerFighters.all.forEach((fighter, index) => {
-						if (index > 5) {
-							return;
-						}
-
-						producer.removeFighter(userId, fighter.uid);
-					});
-				}
-			}
+		producer.addFighter(userId, uid, {
+			name: initialFighter.Oro.Name,
+			level: 0,
+			zone: initialFighter.Name,
 		});
+
+		producer.addActiveFighter(userId, uid);
 	}
 }
