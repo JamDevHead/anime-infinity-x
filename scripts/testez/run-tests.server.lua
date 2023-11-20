cc30fd01696b5959e6480c49b03ab84ab885fe7f
Revolution <tavinho.daniel@gmail.com>
2023-11-20 16:13:57 -0300

    feat: add StarterPlayer service and test path to run-tests.server.lua

--- scripts/testez/run-tests.server.lua
+++ scripts/testez/run-tests.server.lua
@@ -1,9 +1,11 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
+local StarterPlayer = game:GetService("StarterPlayer")
 
 local TestEZ = require(ReplicatedStorage.rbxts_include.node_modules["@rbxts"].testez.src)
 
 TestEZ.TestBootstrap:run({
+    StarterPlayer.StarterPlayerScripts.client.tests,
 	ServerScriptService.server.tests,
 	ReplicatedStorage.shared.tests,
 }, TestEZ.Reporters.TextReporter)
