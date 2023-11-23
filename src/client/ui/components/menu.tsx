103de5fbb82f21ba2d1ed442fc0c21af9b67fa51
Revolution <tavinho.daniel@gmail.com>
2023-11-23 15:04:33 -0300

    feat: add menu click button and better ux

--- src/client/ui/components/menu.tsx
+++ src/client/ui/components/menu.tsx
@@ -1,36 +1,47 @@
-import Roact from "@rbxts/roact";
+import Roact, { FunctionComponent } from "@rbxts/roact";
 import { colors } from "@/client/constants/colors";
+import { Button } from "@/client/ui/components/button";
 import { Frame } from "@/client/ui/components/frame";
 import { Image } from "@/client/ui/components/image";
 import { useRem } from "@/client/ui/hooks/use-rem";
 import { images } from "@/shared/assets/images";
 
-export const Menu = () => {
+type MenuProps = {
+	onClick?: () => void;
+};
+
+export const Menu: FunctionComponent<MenuProps> = ({ onClick }) => {
 	const rem = useRem();
 
 	return (
-		<Frame size={UDim2.fromOffset(rem(3), rem(3))} backgroundTransparency={1}>
+		<Button size={UDim2.fromOffset(rem(2.5), rem(2.5))} backgroundTransparency={1} onClick={onClick}>
 			<Frame
 				size={UDim2.fromScale(1, 1)}
 				backgroundColor={Color3.fromHex("#2F2B66")}
-				cornerRadius={new UDim(0, 4)}
+				cornerRadius={new UDim(0, rem(8, "pixel"))}
 			>
-				<Image image={images.icons.quad_menu} size={UDim2.fromScale(1, 1)} />
+				<Frame
+					size={UDim2.fromScale(1, 1)}
+					backgroundColor={Color3.fromHex("#2F2B66")}
+					cornerRadius={new UDim(0, 4)}
+				>
+					<Image image={images.icons.quad_menu} size={UDim2.fromScale(1, 1)} />
+					<uipadding
+						PaddingLeft={new UDim(0, rem(4, "pixel"))}
+						PaddingRight={new UDim(0, rem(4, "pixel"))}
+						PaddingTop={new UDim(0, rem(4, "pixel"))}
+						PaddingBottom={new UDim(0, rem(4, "pixel"))}
+					/>
+					<uistroke Thickness={2} Transparency={0.5} Color={colors.black} />
+				</Frame>
+
 				<uipadding
 					PaddingLeft={new UDim(0, rem(4, "pixel"))}
 					PaddingRight={new UDim(0, rem(4, "pixel"))}
 					PaddingTop={new UDim(0, rem(4, "pixel"))}
 					PaddingBottom={new UDim(0, rem(4, "pixel"))}
 				/>
-				<uistroke Thickness={2} Transparency={0.5} Color={colors.black} />
 			</Frame>
-
-			<uipadding
-				PaddingLeft={new UDim(0, rem(4, "pixel"))}
-				PaddingRight={new UDim(0, rem(4, "pixel"))}
-				PaddingTop={new UDim(0, rem(4, "pixel"))}
-				PaddingBottom={new UDim(0, rem(4, "pixel"))}
-			/>
-		</Frame>
+		</Button>
 	);
 };
