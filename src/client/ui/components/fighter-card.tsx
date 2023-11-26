99816cf49de9aa8f324a4bf2fda9a02d90ec4454
Revolution <tavinho.daniel@gmail.com>
2023-11-26 12:34:58 -0300

    fix: background not loading for fighters

--- src/client/ui/components/fighter-card.tsx
+++ src/client/ui/components/fighter-card.tsx
@@ -34,7 +34,7 @@ export const FighterCard: FunctionComponent<FighterCardProps> = ({ onClick, acti
 		>
 			<CanvasGroup size={UDim2.fromScale(1, 1)} cornerRadius={new UDim(0, 12)} backgroundTransparency={1}>
 				<Image
-					image={images.ui.cards.backgrounds[zone as keyof typeof images.ui.cards.backgrounds]}
+					image={images.ui.cards.backgrounds[zone.lower() as keyof typeof images.ui.cards.backgrounds]}
 					size={UDim2.fromScale(1, 1)}
 				>
 					{active && (
