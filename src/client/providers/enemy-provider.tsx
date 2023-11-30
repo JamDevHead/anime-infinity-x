f82f7a06216db6505ad773c2b004779496ce27b4
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-30 01:03:47 -0300

    chore: remove enemy aura when active fighters is empty

--- src/client/providers/enemy-provider.tsx
+++ src/client/providers/enemy-provider.tsx
@@ -1,26 +1,34 @@
+import { useSelectorCreator } from "@rbxts/react-reflex";
 import { createPortal } from "@rbxts/react-roblox";
-import Roact from "@rbxts/roact";
+import Roact, { useMemo } from "@rbxts/roact";
 import { selectSelectedEnemiesByPlayerId } from "shared/store/enemy-selection";
 import { EnemyAura } from "@/client/components/react/enemy-aura";
 import { EnemyHover } from "@/client/components/react/enemy-hover";
 import { useRootSelector } from "@/client/store";
 import { selectHoveredEnemy } from "@/client/store/enemy-hover";
+import { selectActivePlayerFighters } from "@/shared/store/players/fighters";
 import { getEnemyModelByUid } from "@/shared/utils/enemies";
 
 function SelectionProvider({ userId }: { userId: string }) {
-	const selectedEnemies = useRootSelector(selectSelectedEnemiesByPlayerId(userId));
+	const selectedEnemies = useSelectorCreator(selectSelectedEnemiesByPlayerId, userId);
+	const activeFighters = useSelectorCreator(selectActivePlayerFighters, userId);
+	const activeFightersIsEmpty = useMemo(() => {
+		print("active fighters changed", activeFighters.size());
+		return activeFighters.size() === 0;
+	}, [activeFighters]);
 
 	return (
 		<>
-			{selectedEnemies?.map((enemyUid) => {
-				const enemy = getEnemyModelByUid(enemyUid);
+			{!activeFightersIsEmpty &&
+				selectedEnemies?.map((enemyUid) => {
+					const enemy = getEnemyModelByUid(enemyUid);
 
-				if (enemy) {
-					return createPortal(<EnemyAura enemy={enemy} />, enemy);
-				} else {
-					return <></>;
-				}
-			})}
+					if (enemy) {
+						return createPortal(<EnemyAura enemy={enemy} />, enemy);
+					} else {
+						return <></>;
+					}
+				})}
 		</>
 	);
 }
@@ -28,7 +36,9 @@ function SelectionProvider({ userId }: { userId: string }) {
 function HoverProvider() {
 	const hoveredEnemyUid = useRootSelector(selectHoveredEnemy);
 
-	const hoveredEnemy = hoveredEnemyUid !== undefined ? getEnemyModelByUid(hoveredEnemyUid) : undefined;
+	const hoveredEnemy = useMemo(() => {
+		return hoveredEnemyUid !== undefined && getEnemyModelByUid(hoveredEnemyUid);
+	}, [hoveredEnemyUid]);
 
 	return <>{hoveredEnemy && createPortal(<EnemyHover />, hoveredEnemy)}</>;
 }
