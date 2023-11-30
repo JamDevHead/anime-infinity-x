5ae965c1e29929163123dfa1df74e67ca409adc6
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-11-30 16:01:45 -0300

    feat: play reward sound on drop collection

--- src/client/providers/enemy-drop-provider.tsx
+++ src/client/providers/enemy-drop-provider.tsx
@@ -1,10 +1,12 @@
 import { useSelectorCreator } from "@rbxts/react-reflex";
 import Roact from "@rbxts/roact";
 import { EnemyDrop } from "@/client/components/react/enemy-drop";
+import { usePlayerId } from "@/client/ui/hooks/use-player-id";
 import { selectEnemiesDropsByOwnerId } from "@/shared/store/enemies/enemies-selectors";
 
-export function EnemyDropProvider({ ownerId }: { ownerId: string }) {
-	const drops = useSelectorCreator(selectEnemiesDropsByOwnerId, ownerId);
+export function EnemyDropProvider() {
+	const userId = usePlayerId();
+	const drops = useSelectorCreator(selectEnemiesDropsByOwnerId, userId);
 
 	return (
 		<>
