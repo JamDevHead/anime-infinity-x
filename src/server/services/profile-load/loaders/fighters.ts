014064042ee09a10cff8531065bd5ff343b058f0
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-02 15:39:57 -0300

    fix(fighter-loader): resolve logic error

--- src/server/services/profile-load/loaders/fighters.ts
+++ src/server/services/profile-load/loaders/fighters.ts
@@ -12,8 +12,8 @@ export function loadFighters(player: Player, profile: Profile<PlayerData>, logge
 	logger.Info("Loading {player} data {@data}", player.Name, profile.Data);
 
 	for (const fighter of fighters.all) {
-		const fighterZone = fighter?.zone === undefined ? fightersFolder.FindFirstChild(fighter.zone) : undefined;
-		const fighterModel = fighter?.name === undefined ? fighterZone?.FindFirstChild(fighter.name) : undefined;
+		const fighterZone = fighter?.zone !== undefined ? fightersFolder.FindFirstChild(fighter.zone) : undefined;
+		const fighterModel = fighter?.name !== undefined ? fighterZone?.FindFirstChild(fighter.name) : undefined;
 
 		if (fighterModel) {
 			continue;
