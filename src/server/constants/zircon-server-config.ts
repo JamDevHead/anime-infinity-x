76910ce8e7b0b9a7bb5695644fa1543b4e038ac4
Revolution <tavinho.daniel@gmail.com>
2023-12-02 18:46:37 -0300

    fix: add zircon missing default user group

--- src/server/constants/zircon-server-config.ts
+++ src/server/constants/zircon-server-config.ts
@@ -3,5 +3,9 @@ import { EnableGizmos } from "@/shared/commands/debug/enable-gizmos";
 import { Print } from "@/shared/commands/print";
 
 export const ZirconServerConfig = ZirconConfigurationBuilder.default()
+	.CreateDefaultCreatorGroup()
+	.CreateDefaultUserGroup({
+		CanAccessConsole: true,
+	})
 	.AddFunction(Print, ["User"])
 	.AddNamespace(new ZirconNamespaceBuilder("debug").AddFunction(EnableGizmos).Build(), ["User"]);
