45792dc0b0db50f60fbaec5cf8afc48b2b828818
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-02 01:24:44 -0300

    chore: apply RequestStreamAroundAsync when zone loading

--- src/server/services/zones-loader.ts
+++ src/server/services/zones-loader.ts
@@ -1,6 +1,6 @@
 import { OnStart, Service } from "@flamework/core";
 import { Logger } from "@rbxts/log";
-import { Players, ReplicatedStorage, Workspace } from "@rbxts/services";
+import { ReplicatedStorage, Workspace } from "@rbxts/services";
 import { OnCharacterAdd } from "@/server/services/lifecycles/on-character-add";
 import { OnPlayerAdd } from "@/server/services/lifecycles/on-player-add";
 import { store } from "@/server/store";
@@ -76,7 +76,7 @@ export class ZonesLoader implements OnStart, OnPlayerAdd, OnCharacterAdd {
 				return;
 			}
 
-			this.spawnOnZone(player.Character, zones.current);
+			this.spawnOnZone(player, player.Character, zones.current);
 		});
 
 		this.zonesSubscriptions.set(player, unsubscribe);
@@ -96,10 +96,10 @@ export class ZonesLoader implements OnStart, OnPlayerAdd, OnCharacterAdd {
 			return;
 		}
 
-		this.spawnOnZone(character, zones.current);
+		this.spawnOnZone(player, character, zones.current);
 	}
 
-	spawnOnZone(character: Model, zoneName: string) {
+	spawnOnZone(player: Player, character: Model, zoneName: string) {
 		const zone = this.zonesFolder.FindFirstChild(zoneName) as Zone | undefined;
 
 		if (!zone) {
@@ -112,16 +112,18 @@ export class ZonesLoader implements OnStart, OnPlayerAdd, OnCharacterAdd {
 			}
 		}
 
-		const player = Players.GetPlayerFromCharacter(character);
-
-		store.setChangingZone(tostring(player?.UserId), true);
+		store.setChangingZone(tostring(player.UserId), true);
 
 		this.logger.Debug("Spawning {@player} on zone {zone}", character.GetFullName(), zoneName);
 
 		const spawnOffset = Vector3.yAxis.mul(3 + zone.Spawn.Size.Y / 2);
-		character.PivotTo(zone.Spawn.CFrame.add(spawnOffset));
+		const spawnCFrame = zone.Spawn.CFrame.add(spawnOffset);
+
+		player.RequestStreamAroundAsync(spawnCFrame.Position, 5);
+
+		character.PivotTo(spawnCFrame);
 
 		//task.wait(1);
-		store.setChangingZone(tostring(player?.UserId), false);
+		store.setChangingZone(tostring(player.UserId), false);
 	}
 }
