f1c6f8e5f0d2169842b05690f95fa34e62da4b60
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-04 12:52:04 -0300

    fix: hurt particle scaling with enemy scale

--- src/client/components/enemy-component.tsx
+++ src/client/components/enemy-component.tsx
@@ -147,6 +147,7 @@ export class Enemy extends EnemyComponent implements OnStart {
 		const hurtPart = new Instance("Part");
 		const hurtBillboard = new Instance("BillboardGui");
 		const hurtLabel = new Instance("TextLabel");
+		const enemyScale = this.instance.GetScale();
 		const FORCE = 5;
 
 		hurtPart.Size = Vector3.one;
@@ -158,7 +159,7 @@ export class Enemy extends EnemyComponent implements OnStart {
 		hurtPart.CanTouch = false;
 		hurtPart.CFrame = this.root.CFrame;
 
-		hurtBillboard.Size = UDim2.fromScale(1, 1);
+		hurtBillboard.Size = UDim2.fromScale(enemyScale * 1.25, enemyScale);
 		hurtBillboard.AlwaysOnTop = true;
 
 		hurtLabel.BackgroundTransparency = 1;
@@ -181,7 +182,7 @@ export class Enemy extends EnemyComponent implements OnStart {
 			),
 		);
 
-		task.delay(0.4, () => {
+		task.delay(enemyScale / 2, () => {
 			hurtPart.Destroy();
 		});
 	}
