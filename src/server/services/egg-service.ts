fb5313be30be9ebf986d9b9ec48075e623988217
Revolution <tavinho.daniel@gmail.com>
2023-12-07 17:48:02 -0300

    feat: add balance check for opening eggs in EggService

--- src/server/services/egg-service.ts
+++ src/server/services/egg-service.ts
@@ -1,9 +1,11 @@
 import { OnStart, Service } from "@flamework/core";
 import { Logger } from "@rbxts/log";
 import Object from "@rbxts/object-utils";
+import { store } from "@/server/store";
 import { addFighterFor, generateStats } from "@/server/utils/fighters";
 import { FighterRarity, Rarity } from "@/shared/constants/rarity";
 import remotes from "@/shared/remotes";
+import { selectPlayerBalance } from "@/shared/store/players";
 
 @Service()
 export class EggService implements OnStart {
@@ -15,7 +17,14 @@ export class EggService implements OnStart {
 		remotes.eggs.open.onRequest((player, zone) => {
 			this.logger.Info(`Player ${player.Name} opened egg ${zone}`);
 
+			const balance = store.getState(selectPlayerBalance(tostring(player.UserId)));
+
+			if (balance === undefined) return;
+
 			const rarityByZone = FighterRarity[(zone.lower() ?? "nrt") as keyof typeof FighterRarity];
+			const zoneIndex = Object.keys(FighterRarity).findIndex((zoneName) => zoneName === zone.lower());
+
+			if (zoneIndex === -1) return;
 
 			if (!rarityByZone) return;
 
@@ -25,7 +34,14 @@ export class EggService implements OnStart {
 				| undefined;
 			if (!rarity) return;
 
-			this.logger.Info(`Player ${player.Name} got ${rarity[0]} from egg ${zone}`);
+			const price = 100 * zoneIndex;
+
+			if (balance.coins < price) return;
+
+			this.logger.Info("Player {@PlayerName} got {@Rarity} from egg {@Zone}", player.Name, rarity[0], zone);
+
+			this.logger.Info("Removing {@Price} coins from {@PlayerName}", price, player.Name);
+			store.removeBalance(tostring(player.UserId), "coins", price);
 
 			return addFighterFor(player, {
 				name: rarity[0],
