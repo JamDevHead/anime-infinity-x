ce773eab359a086b16539be583277c238fa4f8d4
Revolution <tavinho.daniel@gmail.com>
2023-12-07 14:23:18 -0300

    feat: add player data reset command

--- src/server/services/zircon.ts
+++ src/server/services/zircon.ts
@@ -4,6 +4,7 @@ import { ZirconServer } from "@rbxts/zircon";
 import { ZirconNamespace } from "@rbxts/zircon/out/Class/ZirconNamespace";
 import { balanceCommands } from "@/server/commands/balance";
 import { giveCommand } from "@/server/commands/give";
+import { playerDataCommands } from "@/server/commands/player-data";
 import { zoneCommands } from "@/server/commands/zone";
 import { ZirconServerConfig } from "@/server/constants/zircon-server-config";
 
@@ -15,7 +16,8 @@ export class Zircon implements OnInit {
 		this.logger.Info("Zircon service initialized");
 
 		ZirconServer.Registry.Init(
-			ZirconServerConfig.AddNamespace(new ZirconNamespace("zone", zoneCommands), ["Devs"])
+			ZirconServerConfig.AddNamespace(new ZirconNamespace("data", playerDataCommands), ["Devs"])
+				.AddNamespace(new ZirconNamespace("zone", zoneCommands), ["Devs"])
 				.AddNamespace(new ZirconNamespace("balance", balanceCommands), ["Devs"])
 				.AddFunction(giveCommand, ["Devs"])
 				.Build(),
