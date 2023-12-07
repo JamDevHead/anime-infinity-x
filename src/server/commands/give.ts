0a42fc5a2ede951af53e8f84e4b6bc5b50af22b1
Revolution <tavinho.daniel@gmail.com>
2023-12-07 13:53:21 -0300

    feat: add rarity in give command

--- src/server/commands/give.ts
+++ src/server/commands/give.ts
@@ -1,5 +1,7 @@
+import Object from "@rbxts/object-utils";
 import { ZirconEnumBuilder, ZirconFunctionBuilder } from "@rbxts/zircon";
 import { addFighterFor } from "@/server/utils/fighters";
+import { FighterRarity } from "@/shared/constants/rarity";
 
 const ItemType = new ZirconEnumBuilder("ItemType").FromArray(["fighter", "item"]);
 
@@ -23,18 +25,34 @@ export const giveCommand = new ZirconFunctionBuilder("give")
 					return;
 				}
 
+				const rarityByZone = FighterRarity[(zone.lower() ?? "nrt") as keyof typeof FighterRarity];
+
+				if (!rarityByZone) {
+					context.LogError("Invalid zone");
+					return;
+				}
+
+				const rarity = Object.entries(rarityByZone).find(([fighterName]) => fighterName === entityName) as
+					| [string, number]
+					| undefined;
+
+				if (!rarity) {
+					context.LogError("Invalid fighter name");
+					return;
+				}
+
 				addFighterFor(player, {
 					displayName: entityName,
 					name: entityName,
 					zone: zone.upper(),
 					stats: {
-						damage: 1,
-						dexterity: 10,
-						level: 1,
-						sellPrice: 0,
+						damage: math.random() * rarity[1] * 10,
+						dexterity: math.random() * rarity[1] * 10,
+						level: math.random(1, 3),
+						sellPrice: math.random() * rarity[1] * 100,
 						xp: 0,
 					},
-					rarity: 1,
+					rarity: rarity[1],
 				});
 
 				context.LogInfo(`Giving fighter ${entityName} to ${player.Name} ${player.UserId}`);
