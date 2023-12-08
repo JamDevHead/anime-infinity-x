69a7dcbbcf4aede3efa62af23cb865fd5dd89f08
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-07 23:57:18 -0300

    fix: remove fighter model card camera collisions

--- src/client/components/react/fighter-model-card/fighter-model-card.tsx
+++ src/client/components/react/fighter-model-card/fighter-model-card.tsx
@@ -1,5 +1,5 @@
-import { useCamera, useEventListener, useUnmountEffect } from "@rbxts/pretty-react-hooks";
-import Roact, { useEffect, useMemo } from "@rbxts/roact";
+import { useCamera, useEventListener, useLatest, useUnmountEffect } from "@rbxts/pretty-react-hooks";
+import Roact, { useEffect, useMemo, useState } from "@rbxts/roact";
 import { ReplicatedStorage, RunService } from "@rbxts/services";
 import { PlayerFighter } from "@/shared/store/players";
 
@@ -7,6 +7,8 @@ const fightersFolder = ReplicatedStorage.assets.Avatars.FightersModels;
 
 export function FighterModelCard({ fighter }: { fighter: Pick<PlayerFighter, "zone" | "name"> }) {
 	const camera = useCamera();
+	const [collidableParts, setCollidableParts] = useState<BasePart[]>([]);
+	const latestCollidableParts = useLatest(collidableParts);
 	const fighterModel = useMemo(() => {
 		const fighterZone = fightersFolder.FindFirstChild(fighter.zone);
 		const fighterModel = fighterZone?.FindFirstChild(fighter.name);
@@ -19,7 +21,7 @@ export function FighterModelCard({ fighter }: { fighter: Pick<PlayerFighter, "zo
 		() => {
 			const model = fighterModel as Model;
 
-			const scale = 0.06;
+			const scale = 0.18;
 			const offset = new Vector3(0, -0.5, -6);
 			const cameraOffset = new CFrame(offset.mul(scale));
 			const fighterCFrame = camera.CFrame.mul(cameraOffset).mul(CFrame.Angles(0, math.rad(180), 0));
@@ -28,6 +30,12 @@ export function FighterModelCard({ fighter }: { fighter: Pick<PlayerFighter, "zo
 		},
 		{ connected: fighterModel !== undefined },
 	);
+
+	useEventListener(RunService.Stepped, () => {
+		latestCollidableParts.current.forEach((part) => {
+			part.CanCollide = false;
+		});
+	});
 
 	useEffect(() => {
 		if (!fighterModel) {
@@ -39,17 +47,23 @@ export function FighterModelCard({ fighter }: { fighter: Pick<PlayerFighter, "zo
 		animator.Parent = humanoid;
 
 		const [bounding] = fighterModel.GetBoundingBox();
-		fighterModel.GetDescendants().forEach((descendants) => {
-			if (descendants.IsA("BasePart")) {
-				descendants.CastShadow = false;
-				descendants.Anchored = true;
-				descendants.CanCollide = false;
-				descendants.CanQuery = false;
+		const parts = [] as BasePart[];
+
+		fighterModel.GetDescendants().forEach((descendant) => {
+			if (descendant.IsA("BasePart")) {
+				descendant.CastShadow = false;
+				descendant.Anchored = true;
+				descendant.CanCollide = false;
+				descendant.CanQuery = false;
+				parts.push(descendant);
 			}
 		});
+
+		setCollidableParts(parts);
+
 		fighterModel.PrimaryPart = undefined;
 		fighterModel.WorldPivot = bounding;
-		fighterModel.ScaleTo(0.05);
+		fighterModel.ScaleTo(0.15);
 		fighterModel.Parent = camera;
 	}, [camera, fighterModel]);
 
