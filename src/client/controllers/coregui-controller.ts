f644b0384e565a74d989d3eff61c4597eeef2006
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-17 14:50:43 -0300

    feat: remove gameplay paused notification
    resolves #122

--- src/client/controllers/coregui-controller.ts
+++ src/client/controllers/coregui-controller.ts
@@ -1,10 +1,11 @@
 import { Controller, OnStart } from "@flamework/core";
-import { StarterGui } from "@rbxts/services";
+import { GuiService, StarterGui } from "@rbxts/services";
 import { RootState, store } from "@/client/store";
 
 @Controller()
 export class CoreguiController implements OnStart {
 	onStart() {
+		GuiService.SetGameplayPausedNotificationEnabled(false);
 		StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false);
 		const selectHudVisible = (state: RootState) => state.hud.visible;
 
