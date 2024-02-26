fdd87563033060704e8db4a33d41ef3688d3533d
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-02-25 22:04:04 -0300

    refactor: remove old fighter tracker code

--- src/client/components/fighter-goal/fighter-goal-types.ts
+++
@@ -1,8 +0,0 @@
-export interface FighterGoalAttributes {
-	fighterId: string;
-	characterId: string;
-	playerId: number;
-	goalOffset: CFrame;
-}
-
-export type FighterGoalInstance = Part & { weld: WeldConstraint };
